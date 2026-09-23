b582f7627e0a0392f71963401fd820291f308bf8
MGODamian <71386490+MGODamian@users.noreply.github.com>
2026-09-23 12:05:20 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/eredivisie-voorspellen`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/wedstrijden`,
       lastModified: new Date(),
       changeFrequency: "daily",
