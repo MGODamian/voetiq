f5b56b1e5acfbc06e732f4ff01bebde76fc63666
MGODamian <71386490+MGODamian@users.noreply.github.com>
2026-09-23 12:39:54 +0200

    Add Bundesliga predictions URL to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -41,6 +41,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/bundesliga-voorspellen`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/wedstrijden`,
       lastModified: new Date(),
       changeFrequency: "daily",
