585a8791a23dd0e8aa4d9e82c8beb89850fe23f1
MGODamian <71386490+MGODamian@users.noreply.github.com>
2026-09-23 12:03:51 +0200

    Add new URL entry to sitemap with weekly frequency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/hoe-werkt-het`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/ranglijst`,
       lastModified: new Date(),
       changeFrequency: "daily",
